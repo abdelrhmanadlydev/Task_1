6b056799d6cbc9937cd82cca218ffa57b30f4e8e
Abdo Adly <abdelrhman.mohamed@intouchegypt.com>
2025-09-30 23:23:09 +0300

    Add project files.

---
+++ Task_1/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task_1
+{
+    [Flags]
+    internal enum Food
+    {
+        Pizza  =1,
+        Burger =2,
+        Salad  =4,
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isParsed;
+            string input;
+            do
+            {
+
+                Console.WriteLine("enter their favorite food");
+
+                input = Console.ReadLine();
+                isParsed = Enum.TryParse<Food>(input, out Food food);
+
+                if (isParsed)
+                {
+                    Console.WriteLine($"You chose: {food}");
+                }
+                else
+                {
+                    Console.WriteLine("That food is not on the menu!");
+                }
+            }while (!isParsed);
+        }
+
+    }
+}
